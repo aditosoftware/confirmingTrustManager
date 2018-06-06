dd1dc7bbf44dfe4414cbfbad7689fe9cdb46c9df
j.boesl <j.boesl@adito.de>
2018-06-06 16:05:52 +0200

    TrustManager now extends X509ExtendedTrustManager to be able to display the connection target. Extended the confirmation dialog to display a simple dialog first.

--- src/main/java/de/adito/trustmanager/ConfirmingUITrustManager.java
+++ src/main/java/de/adito/trustmanager/ConfirmingUITrustManager.java
@@ -5,71 +5,105 @@ import de.adito.trustmanager.store.ICustomTrustStore;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.security.*;
+import java.security.cert.*;
 
-import static java.lang.String.format;
+public class ConfirmingUITrustManager extends CustomTrustManager
+{
 
-public class ConfirmingUITrustManager extends CustomTrustManager {
+  public ConfirmingUITrustManager(ICustomTrustStore pTrustStore) throws NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, InvalidAlgorithmParameterException
+  {
+    super(pTrustStore);
+  }
 
-    public ConfirmingUITrustManager(ICustomTrustStore pTrustStore) throws NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, InvalidAlgorithmParameterException {
-        super(pTrustStore);
+  protected boolean checkCertificateAndShouldPersist(X509Certificate[] chain, CertificateException e, String pSimpleInfo)
+      throws CertificateException
+  {
+    JTextArea textArea = new JTextArea(
+        "The owner of '" + pSimpleInfo + "' has configured their website improperly. " +
+            "To protect your information from being stolen the connection to the target is interrupted.");
+    textArea.setEditable(false);
+    textArea.setOpaque(false);
+    textArea.setLineWrap(true);
+    textArea.setWrapStyleWord(true);
+    JScrollPane scrollPane = new JScrollPane(textArea);
+    scrollPane.setBorder(null);
+    scrollPane.setPreferredSize(new Dimension(640, 400));
+
+
+    String[] options = new String[]{
+        "Cancel",
+        "Advanced"
+    };
+    int r = JOptionPane.showOptionDialog(
+        null, scrollPane,
+        "Your connection is not secure",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+        null, options, options[0]);
+
+    switch (r) {
+      case 1:
+        return _extendedDialog(chain, e, pSimpleInfo);
+      case 0:
+      default:
+        throw e;
     }
+  }
 
-    protected boolean checkCertificateAndShouldPersist(
-            X509Certificate[] chain, CertificateException e)
-            throws CertificateException {
+  protected boolean _extendedDialog(X509Certificate[] chain, CertificateException e, String pSimpleInfo) throws CertificateException
+  {
+    String[] options = new String[]{
+        "ignore",
+        "import",
+        "deny"
+    };
+    String dn = chain[0].getSubjectDN().getName();
+    String caDN = chain[0].getIssuerX500Principal().getName();
+    String msg = String.format(
+        e.getLocalizedMessage() + "\n" +
+            "\n" +
+            "subject common name: %1$s\n" +
+            "subject organization name: %2$s\n" +
+            "principal common name: %3$s\n" +
+            "principal organziation name: %4$s\n" +
+            "valid since: %5$s\n" +
+            "valid till: %6$s\n" +
+            "sha1 hash: %7$s\n" +
+            "md5 hash: %8$s\n" +
+            "\n" +
+            "%9$s",
+        TrustManagerUtil.parseDN(dn, "cn"),
+        TrustManagerUtil.parseDN(dn, "o"),
+        TrustManagerUtil.parseDN(caDN, "cn"),
+        TrustManagerUtil.parseDN(caDN, "o"),
+        chain[0].getNotBefore(),
+        chain[0].getNotAfter(),
+        TrustManagerUtil.hashSHA1(chain[0]),
+        TrustManagerUtil.hashMD5(chain[0]),
+        chain[0].toString());
 
-        String[] options;
-        options = new String[]{
-                "yes once",
-                "yes always",
-                "no"
-        };
-        String dn = chain[0].getSubjectDN().getName();
-        String caDN = chain[0].getIssuerX500Principal().getName();
-        String msg = format(e.getLocalizedMessage() + "\n" +
-                        "\n" +
-                        "subject common name: %1$s\n" +
-                        "subject organization name: %2$s\n" +
-                        "principal common name: %3$s\n" +
-                        "principal organziation name: %4$s\n" +
-                        "valid since: %5$s\n" +
-                        "valid till: %6$s\n" +
-                        "sha1 hash: %7$s\n" +
-                        "md5 hash: %8$s\n" +
-                        "\n" +
-                        "%9$s",
-                TrustManagerUtil.parseDN(dn, "cn"),
-                TrustManagerUtil.parseDN(dn, "o"),
-                TrustManagerUtil.parseDN(caDN, "cn"),
-                TrustManagerUtil.parseDN(caDN, "o"),
-                chain[0].getNotBefore(),
-                chain[0].getNotAfter(),
-                TrustManagerUtil.hashSHA1(chain[0]),
-                TrustManagerUtil.hashMD5(chain[0]),
-                chain[0].toString());
+    JTextArea textArea = new JTextArea(msg);
+    textArea.setEditable(false);
+    textArea.setOpaque(false);
+    textArea.setLineWrap(true);
+    textArea.setWrapStyleWord(true);
+    JScrollPane scrollPane = new JScrollPane(textArea);
+    scrollPane.setBorder(null);
+    scrollPane.setPreferredSize(new Dimension(640, 400));
 
-        JTextArea textArea = new JTextArea(msg);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        scrollPane.setPreferredSize(new Dimension(640, 400));
+    int r = JOptionPane.showOptionDialog(
+        null, scrollPane,
+        "certificate of '" + pSimpleInfo + "' is not trusted",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+        null, options, options[2]);
 
-        int r = JOptionPane.showOptionDialog(null, scrollPane,
-                "certificate is not trusted",
-                JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
-                null, options, options[1]);
-        switch (r) {
-            case 0:
-                return false;
-            case 1:
-                return true;
-            default:
-                throw e;
-        }
+    switch (r) {
+      case 0:
+        return false;
+      case 1:
+        return true;
+      default:
+        throw e;
     }
+  }
 }
--- src/main/java/de/adito/trustmanager/CustomTrustManager.java
+++ src/main/java/de/adito/trustmanager/CustomTrustManager.java
@@ -1,104 +1,140 @@
 package de.adito.trustmanager;
 
 import de.adito.trustmanager.store.ICustomTrustStore;
-import sun.security.validator.ValidatorException;
 
-import javax.net.ssl.CertPathTrustManagerParameters;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.*;
+import java.security.*;
 import java.security.cert.*;
 import java.util.EnumSet;
 
-public abstract class CustomTrustManager implements X509TrustManager {
-    private final X509TrustManager defaultTrustManager;
-    private ICustomTrustStore trustStore;
+public abstract class CustomTrustManager extends X509ExtendedTrustManager
+{
+  private final X509ExtendedTrustManager defaultTrustManager;
+  private ICustomTrustStore trustStore;
 
 
-    public CustomTrustManager(ICustomTrustStore pTrustStore) throws NoSuchAlgorithmException, KeyStoreException, IOException,
-            CertificateException, InvalidAlgorithmParameterException {
-        trustStore = pTrustStore;
+  public CustomTrustManager(ICustomTrustStore pTrustStore) throws NoSuchAlgorithmException, KeyStoreException, IOException,
+      CertificateException, InvalidAlgorithmParameterException
+  {
+    trustStore = pTrustStore;
 
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+    TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
 
-        // initialize certification path checking for the offered certificates and revocation checks against CLRs
-        CertPathBuilder cpb = CertPathBuilder.getInstance("PKIX");
-        PKIXRevocationChecker rc = (PKIXRevocationChecker) cpb.getRevocationChecker();
-        rc.setOptions(EnumSet.of(
-                PKIXRevocationChecker.Option.PREFER_CRLS, // prefer CLR over OCSP
-                PKIXRevocationChecker.Option.ONLY_END_ENTITY,
-                PKIXRevocationChecker.Option.SOFT_FAIL, // handshake should not fail when CRL is not available
-                PKIXRevocationChecker.Option.NO_FALLBACK)); // don't fall back to OCSP checking
+    // initialize certification path checking for the offered certificates and revocation checks against CLRs
+    CertPathBuilder cpb = CertPathBuilder.getInstance("PKIX");
+    PKIXRevocationChecker rc = (PKIXRevocationChecker) cpb.getRevocationChecker();
+    rc.setOptions(EnumSet.of(
+        PKIXRevocationChecker.Option.PREFER_CRLS, // prefer CLR over OCSP
+        PKIXRevocationChecker.Option.ONLY_END_ENTITY,
+        PKIXRevocationChecker.Option.SOFT_FAIL, // handshake should not fail when CRL is not available
+        PKIXRevocationChecker.Option.NO_FALLBACK)); // don't fall back to OCSP checking
 
-        String keyStorePath = System.getProperty("javax.net.ssl.keyStore");
-        if (keyStorePath == null) {
-            String securityPath = System.getProperty("java.home") + File.separator + "lib" + File.separator + "security" + File.separator;
-            if (Files.isRegularFile(Paths.get(securityPath + "jssecacerts")))
-                keyStorePath = securityPath + "jssecacerts";
-            else if (Files.isRegularFile(Paths.get(securityPath + "cacerts")))
-                keyStorePath = securityPath + "cacerts";
-        }
-        String keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword", "changeit");
-        KeyStore ks = KeyStore.getInstance("JKS");
-        TrustManagerUtil.loadKeyStore(ks, keyStorePassword, keyStorePath == null ? null : Paths.get(keyStorePath));
-
-        PKIXBuilderParameters pkixParams = new PKIXBuilderParameters(ks, new X509CertSelector());
-        pkixParams.addCertPathChecker(rc);
-
-        tmf.init(new CertPathTrustManagerParameters(pkixParams));
-
-        javax.net.ssl.TrustManager[] tm = tmf.getTrustManagers();
-        if (tm.length == 0)
-            throw new IllegalStateException("No trust managers found");
-        defaultTrustManager = (X509TrustManager) tm[0];
+    String keyStorePath = System.getProperty("javax.net.ssl.keyStore");
+    if (keyStorePath == null) {
+      String securityPath = System.getProperty("java.home") + File.separator + "lib" + File.separator + "security" + File.separator;
+      if (Files.isRegularFile(Paths.get(securityPath + "jssecacerts")))
+        keyStorePath = securityPath + "jssecacerts";
+      else if (Files.isRegularFile(Paths.get(securityPath + "cacerts")))
+        keyStorePath = securityPath + "cacerts";
     }
+    String keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword", "changeit");
+    KeyStore ks = KeyStore.getInstance("JKS");
+    TrustManagerUtil.loadKeyStore(ks, keyStorePassword, keyStorePath == null ? null : Paths.get(keyStorePath));
 
-    public void checkClientTrusted(X509Certificate[] chain, String authType) {
-        throw new UnsupportedOperationException("checkClientTrusted");
+    PKIXBuilderParameters pkixParams = new PKIXBuilderParameters(ks, new X509CertSelector());
+    pkixParams.addCertPathChecker(rc);
+
+    tmf.init(new CertPathTrustManagerParameters(pkixParams));
+
+    javax.net.ssl.TrustManager[] tm = tmf.getTrustManagers();
+    if (tm.length == 0)
+      throw new IllegalStateException("No trust managers found");
+    defaultTrustManager = (X509ExtendedTrustManager) tm[0];
+  }
+
+  public X509Certificate[] getAcceptedIssuers()
+  {
+    return defaultTrustManager.getAcceptedIssuers();
+  }
+
+  public void checkClientTrusted(X509Certificate[] chain, String authType)
+  {
+    throw new UnsupportedOperationException("checkClientTrusted");
+  }
+
+  @Override
+  public void checkClientTrusted(X509Certificate[] chain, String authType, Socket pSocket)
+  {
+    throw new UnsupportedOperationException("checkClientTrusted");
+  }
+
+  @Override
+  public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine pSSLEngine)
+  {
+    throw new UnsupportedOperationException("checkClientTrusted");
+  }
+
+  public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException
+  {
+    try {
+      defaultTrustManager.checkServerTrusted(chain, authType);
     }
-
-    public X509Certificate[] getAcceptedIssuers() {
-        return defaultTrustManager.getAcceptedIssuers();
+    catch (CertificateException e) {
+      _handleCertificateException(chain, e, null);
     }
+  }
 
-    public void checkServerTrusted(X509Certificate[] chain, String authType)
-            throws CertificateException {
-        try {
-            defaultTrustManager.checkServerTrusted(chain, authType);
-        } catch (CertificateException e) {
-            if (chain == null || chain.length == 0)
-                throw e;
-            if (e instanceof ValidatorException) {
-                Throwable cause = e.getCause();
-                if (cause instanceof CertPathValidatorException) {
-                    Throwable rootCause = cause.getCause();
-                    if (rootCause instanceof CertificateExpiredException || rootCause instanceof CertificateRevokedException)
-                        throw e;
-                }
-            }
-            tryCustomTrustManager(chain, e);
-        }
+  @Override
+  public void checkServerTrusted(X509Certificate[] chain, String authType, Socket pSocket) throws CertificateException
+  {
+    try {
+      defaultTrustManager.checkServerTrusted(chain, authType, pSocket);
     }
-
-    private void tryCustomTrustManager(X509Certificate[] chain, CertificateException e)
-            throws CertificateException {
-        {
-            X509Certificate certificate = chain[chain.length - 1];
-            String alias = TrustManagerUtil.hashSHA1(certificate);
-            if (trustStore.get(alias) != null)
-                return;
-            boolean persist = checkCertificateAndShouldPersist(chain, e);
-            trustStore.add(alias, certificate, persist);
-        }
+    catch (CertificateException e) {
+      _handleCertificateException(chain, e, pSocket.getInetAddress().getHostName());
     }
+  }
 
-    protected abstract boolean checkCertificateAndShouldPersist(X509Certificate[] pChain, CertificateException pE)
-            throws CertificateException;
+  @Override
+  public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine pSSLEngine) throws CertificateException
+  {
+    try {
+      defaultTrustManager.checkServerTrusted(chain, authType, pSSLEngine);
+    }
+    catch (CertificateException e) {
+      _handleCertificateException(chain, e, pSSLEngine.getPeerHost());
+    }
+  }
+
+  private void _handleCertificateException(X509Certificate[] pChain, CertificateException pE, String pSimpleInfo) throws CertificateException
+  {
+    if (pChain == null || pChain.length == 0)
+      throw pE;
+    Throwable cause = pE.getCause();
+    if (cause instanceof CertPathValidatorException) {
+      Throwable rootCause = cause.getCause();
+      if (rootCause instanceof CertificateRevokedException)
+        throw pE;
+    }
+    tryCustomTrustManager(pChain, pE, pSimpleInfo);
+  }
+
+  private void tryCustomTrustManager(X509Certificate[] chain, CertificateException e, String pSimpleInfo)
+      throws CertificateException
+  {
+    {
+      X509Certificate certificate = chain[chain.length - 1];
+      String alias = TrustManagerUtil.hashSHA1(certificate);
+      if (trustStore.get(alias) != null)
+        return;
+      boolean persist = checkCertificateAndShouldPersist(chain, e, pSimpleInfo);
+      trustStore.add(alias, certificate, persist);
+    }
+  }
+
+  protected abstract boolean checkCertificateAndShouldPersist(X509Certificate[] pChain, CertificateException pE, String pSimpleInfo)
+      throws CertificateException;
 }
