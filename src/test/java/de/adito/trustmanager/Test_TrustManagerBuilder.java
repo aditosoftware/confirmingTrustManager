a784fb6487247a27cb0e3127ea792021a6fa1723
Marie Schindlbeck <m.schindlbeck@adito.de>
2018-09-06 10:40:35 +0200

    Added methods to test different operatingSystems

--- src/test/java/de/adito/trustmanager/Test_TrustManagerBuilder.java
+++ src/test/java/de/adito/trustmanager/Test_TrustManagerBuilder.java
@@ -1,6 +1,6 @@
 package de.adito.trustmanager;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -16,26 +16,53 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class Test_TrustManagerBuilder {
 
+    @BeforeEach
+    public void resetSystemProperties()
+    {
+        System.clearProperty("javax.net.ssl.trustStore");
+        System.clearProperty("javax.net.ssl.trustStoreType");
+    }
+
     @Test
     public void testBuildDefaultJavaTrustManager()
             throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, KeyStoreException, IOException
     {
         X509ExtendedTrustManager trustManager = TrustManagerBuilder.buildDefaultTrustManager();
-        assertNotNull(trustManager);
+        assertNotNull(trustManager, "Default TrustManager was not created");
     }
 
     @Test
     @EnabledOnOs(OS.WINDOWS)
-    public void testBuildDefaultTrustManagerSystemProperties() throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, KeyStoreException, IOException {
+    public void testBuildDefaultTrustManagerSystemProperties()
+            throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, KeyStoreException, IOException
+    {
         System.setProperty("javax.net.ssl.trustStore", "NUL");
         System.setProperty("javax.net.ssl.trustStoreType", "Windows-ROOT");
         X509ExtendedTrustManager trustManager = TrustManagerBuilder.buildDefaultTrustManager();
-        assertNotNull(trustManager);
+        assertNotNull(trustManager, "TrustManager with SystemProperties was not created");
     }
 
-    @AfterEach
-    public void resetSystemProperties(){
-        System.clearProperty("javax.net.ssl.trustStore");
-        System.clearProperty("javax.net.ssl.trustStoreType");
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    public void testWindowsTrustManager()
+            throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, KeyStoreException, IOException
+    {
+         assertNotNull(TrustManagerBuilder.buildOSTrustStore(System.getProperty("os.name")), "WindowsTrustManager was not created");
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    public void testMacTrustManager()
+            throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, KeyStoreException, IOException
+    {
+        assertNotNull(TrustManagerBuilder.buildOSTrustStore(System.getProperty("os.name")), "MacTrustManager was not created");
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    public void testLinuxTrustManager()
+            throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, KeyStoreException, IOException
+    {
+        assertNotNull(TrustManagerBuilder.buildOSTrustStore(System.getProperty("os.name")), "LinuxTrustManager was not created");
     }
 }
